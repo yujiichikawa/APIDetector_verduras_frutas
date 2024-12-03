a3ca30d9d1d0aba86a7735308f9b2fa45f31d25d
thiago yuji <ythiago0000@gmail.com>
2024-12-03 12:15:00 -0300

    atualizando para o banco mongodb

--- api.py
+++ api.py
@@ -1,17 +1,29 @@
 from flask import Flask, request, jsonify, render_template
 from ultralytics import YOLO
 from PIL import Image
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://ythiago0000:6aLcl2e4XD0F2Bxo@cluster0.ibuk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client['Caixa']
+catalog_collection = db['catalog']
 
 app = Flask(__name__)
 
 model = YOLO('C:/Users/user\PycharmProjects/APIDetector_verduras_frutas/peso/best.pt')
 
 cart = []
-
-catalog = {
-    'onion': {'price_per_kg': 3.0},
-    'potato':{'price_per_kg': 5.0}
-}
 
 @app.route('/')
 def index():
@@ -32,20 +44,20 @@ def predict():
     for detection in detections:
         class_id = int(detection[5])
         class_name = model.names[class_id]
-        if class_name in catalog:
+        product = catalog_collection.find_one({'name': class_name})
+        if product:
             existing_product = next((item for item in cart if item['name'] == class_name), None)
             if existing_product:
                 existing_product['quantity'] += 1
-                existing_product['total_price'] += catalog[class_name]['price_per_kg']
+                existing_product['total_price'] += product['price_per_kg']
             else:
-                product = {
+                cart.append({
                     'name': class_name,
-                    'price_per_kg': catalog[class_name]['price_per_kg'],
+                    'price_per_kg': product['price_per_kg'],
                     'weight': 1.0,
                     'quantity': 1,
-                    'total_price': catalog[class_name]['price_per_kg'] * 1.0
-                }
-                cart.append(product)
+                    'total_price': product['price_per_kg'] * 1.0
+                })
 
     return jsonify({'cart': cart})
 
